0264e922e91b79c17b0d07e04a03e8b66995f445
Spicoceles <Spicoceles@gmail.com>
2021-06-22 21:13:32 +0100

    pish

---
+++ script/c69077707.lua
@@ -0,0 +1,10 @@
+-- Primordial Awakening
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Chain
+	local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_ACTIVATE)
+		e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+
+end
